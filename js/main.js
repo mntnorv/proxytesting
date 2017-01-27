014f144e7aea892a54263b21020bf3f8fa8dc873
Mantas Norvaiša <mntnorv@gmail.com>
2017-01-28 00:15:02 +0200

    Add some more test cases

--- js/main.js
+++ js/main.js
@@ -23,5 +23,20 @@ $(function() {
   //============================================================================
 
   $('#remote-jquery-img').attr('src', 'img/check.png');
+  $('#remote-jquery-create-img').append($('<img src="img/check.png" />'));
+  $.get('partials/check.html', function (data) {
+    $('#remote-jquery-ajax-img').html(data);
+  });
+
   document.getElementById('remote-js-img').src = 'img/check.png';
+
+  function reqListener () {
+    document.getElementById('remote-js-ajax-img').innerHTML =
+      this.responseText;
+  }
+
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', reqListener);
+  oReq.open('GET', 'partials/check.html');
+  oReq.send();
 });
